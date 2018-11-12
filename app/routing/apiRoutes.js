9e3f18f46102285cccf492f1c2467ce8f68f4dd6
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2018-11-12 01:53:41 -0600

    score differences

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-// const path = require("path");
+const fs = require("fs");
 let friendData = require("../data/friends.json");
 
 module.exports = function(app) {
@@ -6,9 +6,34 @@ module.exports = function(app) {
         res.json(friendData);
     });
 
+    app.get("/api/friends/:name", (req, res) => {
+        let friendMatch = req.params.name;
+        friendData.find((friend) => { 
+            if(friend.name === friendMatch) res.send(friend);
+        });
+    });
+
     app.post("/api/friends", (req, res) => {
         let newFriend = req.body;
         friendData.push(newFriend);
+        // console.log(__dirname);
         res.json(newFriend);
+        /*fs.writeFile("../data/friends.json", friendData, "utf8", (err) => {
+            if(err) throw err;
+            console.log("friends.json ammended");
+        });*/
+        let scoreDiff;
+        for(let friend in friendData){
+            if(friend.name !== newFriend.name){
+                let thisFriendScores = friendData.friend.scores;
+                let thisDiff;
+                for(i=0; i < thisFriendScores.length; i++){
+                    thisDiff += Math.abs(parseInt(thisFriendScores[i]) - parseInt(newFriend.scores[i]));
+                }
+                (!scoreDiff)? scoreDiff = thisDiff :
+                (thisDiff < scoreDiff)? scoreDiff = thisDiff : scoreDiff = scoreDiff;
+                console.log(`ScoreDiffs = ${scoreDiffs}`);
+            }
+        }
     });
 }
