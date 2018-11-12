ad23d9dc10282caa8672c92a0d5c77cef1b53fe7
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2018-11-12 02:48:52 -0600

    updated port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT | 3020;
+const PORT = process.env.PORT;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
