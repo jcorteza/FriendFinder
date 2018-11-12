b04dab1ca6f34e815ae9e6e337c99f629ba66ec1
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2018-11-12 02:42:31 -0600

    working

--- app/public/survey.js
+++ app/public/survey.js
@@ -1,7 +1,8 @@
 $(document).ready(() => {
     $('#survey').scrollspy({ target: '#survey-nav' });
 
-    $("#makeFriend").on("click", () => {
+    $("#makeFriend").on("click", (event) => {
+        event.preventDefault();
 
         let surveyScores = [];
         surveyScores.push($("input[name = 'social']:checked").val());
@@ -21,7 +22,31 @@ $(document).ready(() => {
             scores: surveyScores
         }
 
-        $.post("/api/friends", newFriend);
+        $.post("/api/friends", newFriend, (data) => {
+            $.get("/api/friends", (friendData) => {
+                let scoreDiff;
+                let friendMatch;
+                for(let friend in friendData){
+                    if(friendData[friend].name !== newFriend.name){
+                        let thisFriendScores = friendData[friend].scores;
+                        let thisDiff = 0;
+                        for(i=0; i < thisFriendScores.length; i++){
+                            thisDiff += Math.abs(parseInt(thisFriendScores[i]) - parseInt(newFriend.scores[i]));
+                        }
+                        if (!scoreDiff) {
+                            scoreDiff = thisDiff;
+                            friendMatch = friendData[friend];
+                        }
+                        else if(thisDiff < scoreDiff) {
+                            scoreDiff = thisDiff;
+                            friendMatch = friendData[friend];
+                        }
+                    }
+                }
+                $("#friendName").text(friendMatch.name);
+                $("#friendPic").attr("src", friendMatch.photo);
+            })
+        });
     });
 
 });
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,25 +15,5 @@ module.exports = function(app) {
             if(err) throw err;
             console.log("friends.json ammended");
         });*/
-        let scoreDiff;
-        let friendMatch;
-        for(let friend in friendData){
-            if(friend.name !== newFriend.name){
-                let thisFriendScores = friendData.friend.scores;
-                let thisDiff;
-                for(i=0; i < thisFriendScores.length; i++){
-                    thisDiff += Math.abs(parseInt(thisFriendScores[i]) - parseInt(newFriend.scores[i]));
-                }
-                if (!scoreDiff) {
-                    scoreDiff = thisDiff;
-                    friendMatch = friendData.friend;
-                }
-                else if(thisDiff < scoreDiff) {
-                    scoreDiff = thisDiff;
-                    friendMatch = friendData.friend;
-                }
-            }
-        }
-        console.log(friendMatch);
     });
 }
